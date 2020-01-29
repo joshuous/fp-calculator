56d59be52775083aeca9539cbdf91717dc159f00
Josh Choo <dev@joshuous.com>
2020-01-30 00:00:28 +0800

    implement numbers action

--- src/index.js
+++ src/index.js
@@ -8,10 +8,37 @@ function calculate(
 ) {
   let display = currDisplay;
   let expression = currExpression;
+
+  if (isNumber(newAction)) {
+    switch (true) {
+      case prevAction === 'ac':
+      case prevAction === '=':
+        display = newAction;
+        expression = '';
+        break;
+      case prevAction === '0' && display === '0':
+      case isOperator(prevAction):
+        display = newAction;
+        break;
+      case isNumber(prevAction):
+      case prevAction === '.':
+        display = display.concat(newAction);
+        break;
+    }
+  }
+
   return {
     display: display,
     expression: expression,
   };
+}
+
+function isNumber(c) {
+  return '0123456789'.includes(c);
+}
+
+function isOperator(c) {
+  return '+-/*'.includes(c);
 }
 
 module.exports = calculate;
--- tests/test.js
+++ tests/test.js
@@ -18,3 +18,82 @@ describe('given new action is "ac"', () => {
     expect(result.expression).toBe('');
   });
 });
+
+describe('given new action is a number', () => {
+  describe('given previous action is "ac"', () => {
+    test('display should be the number, and expression should be blank', () => {
+      const result1 = calc('0', 'ac', '0', '');
+      expect(result1.display).toBe('0');
+      expect(result1.expression).toBe('');
+
+      const result2 = calc('9', 'ac', '0', '');
+      expect(result2.display).toBe('9');
+      expect(result2.expression).toBe('');
+    });
+  });
+
+  describe('given previous action is "="', () => {
+    test('display should be the number, and expression should be blank', () => {
+      const result1 = calc('0', '=', '10', '1+2+3+4=');
+      expect(result1.display).toBe('0');
+      expect(result1.expression).toBe('');
+
+      const result2 = calc('7', '=', '6', '2*3=');
+      expect(result2.display).toBe('7');
+      expect(result2.expression).toBe('');
+    });
+  });
+
+  describe('given previous action is number 0', () => {
+    describe('given current display is "0"', () => {
+      test('display should be the number, and expression should be unchanged', () => {
+        const result1 = calc('0', '0', '0', '1+2+');
+        expect(result1.display).toBe('0');
+        expect(result1.expression).toBe('1+2+');
+
+        const result2 = calc('3', '0', '0', '1+2+');
+        expect(result2.display).toBe('3');
+        expect(result2.expression).toBe('1+2+');
+      });
+    });
+    describe('given current display is a number that is not "0"', () => {
+      test('display should be appended with the number, and expression should be unchanged', () => {
+        const result = calc('0', '0', '120', '3*6-');
+        expect(result.display).toBe('1200');
+        expect(result.expression).toBe('3*6-');
+      });
+    });
+  });
+
+  describe('given previous action is a number in range 1 to 9', () => {
+    test('display should be appended with the number, and expression should be unchanged', () => {
+      const result1 = calc('0', '9', '129', '3*6-');
+      expect(result1.display).toBe('1290');
+      expect(result1.expression).toBe('3*6-');
+
+      const result2 = calc('7', '1', '311', '3*6-');
+      expect(result2.display).toBe('3117');
+      expect(result2.expression).toBe('3*6-');
+    });
+  });
+
+  describe('given previous action is a decimal point', () => {
+    test('display should be appended with the number, and expression should be unchanged', () => {
+      const result = calc('0', '.', '74.', '1+3-');
+      expect(result.display).toBe('74.0');
+      expect(result.expression).toBe('1+3-');
+    });
+  });
+
+  describe('given previous action is an operator', () => {
+    test('display should be the number, and expression should be unchanged', () => {
+      const result1 = calc('1', '+', '10', '1+2+3+4+');
+      expect(result1.display).toBe('1');
+      expect(result1.expression).toBe('1+2+3+4+');
+
+      const result2 = calc('0', '/', '7', '1+2*3/');
+      expect(result2.display).toBe('0');
+      expect(result2.expression).toBe('1+2*3/');
+    });
+  });
+});
